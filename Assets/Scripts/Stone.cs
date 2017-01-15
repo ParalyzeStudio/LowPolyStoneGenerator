1faf852bd2ee9e664f182ad21217ccfe59691ea8
ParalyzeStudio <oliviermarty.dev@gmail.com>
2017-01-15 02:54:01 +0100

    Simple bisection works manually

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -8,7 +8,7 @@ public class Stone : MonoBehaviour
 {
     //public int m_vertexCount;
 
-    private PendingMesh m_pendingMesh;
+    private Triangle[] m_pendingTriangles;
     private Mesh m_mesh;
 
     public void Start()
@@ -22,27 +22,32 @@ public class Stone : MonoBehaviour
         }
 
         BuildHull();
-    }
 
-    /**
-    * Use this class to store information about a mesh when constructing it and before applying it to an actual Unity Mesh
-    **/
-    private class PendingMesh
-    {
-        public List<Vector3> m_vertices { get; set; }
-        public List<int> m_triangles { get; set; }
-        public List<Color> m_colors { get; set; }
+        BisectOnce();
 
-        public PendingMesh()
-        {
+        //tmp test triangle/plane intersection
+        //Vector3[] points = new Vector3[3];
+        //points[0] = Vector3.zero;
+        //points[1] = new Vector3(0, 3, 1);
+        //points[2] = new Vector3(5, -1, 3);
 
-        }
+        //Plane plane = new Plane(Vector3.up, points[0]);
+
+        //Triangle triangle = new Triangle(points, null);
+        //Vector3[] intersections;
+        //if (triangle.IntersectsPlane(plane, out intersections))
+        //{
+        //    for (int p = 0; p != intersections.Length; p++)
+        //    {
+        //        Debug.Log("intersection:" + intersections[p]);
+        //    }
+        //}
     }
 
 	public void BuildHull()
     {
         //first start by spherifying a cube
-        m_pendingMesh = SpherifyCube();
+        SpherifyCube();
 
         InvalidateMesh();
     }
@@ -78,12 +83,54 @@ public class Stone : MonoBehaviour
         /**
         * make this square face as a part of a sphere by sampling new vertices inside it
         **/
-        public void Spherify(int subdivisions, out List<Vector3> vertices, out List<int> triangles)
-        {
-            int vertexCount = (subdivisions + 1) * (subdivisions + 1) * 2 * 3;
-            int trianglesCount = vertexCount;
+        //public void Spherify(int subdivisions, out List<Vector3> vertices, out List<int> triangles)
+        //{
+        //    int vertexCount = (subdivisions + 1) * (subdivisions + 1) * 2 * 3;
+        //    int trianglesCount = vertexCount;
 
-            vertices = new List<Vector3>(vertexCount);
+        //    vertices = new List<Vector3>(vertexCount);
+
+        //    for (int i = 0; i != subdivisions + 1; i++)
+        //    {
+        //        for (int j = 0; j != subdivisions + 1; j++)
+        //        {
+        //            Vector3 v1 = m_A;
+        //            v1 += i / (float)(subdivisions + 1) * (m_D - m_A);
+        //            v1 += j / (float)(subdivisions + 1) * (m_B - m_A);
+
+        //            Vector3 v2 = v1 + (1 / (float)(subdivisions + 1)) * ((m_D - m_A) + (m_B - m_A));
+        //            Vector3 v3 = v1 + (1 / (float)(subdivisions + 1)) * (m_B - m_A);
+        //            Vector3 v4 = v1;
+        //            Vector3 v5 = v1 + (1 / (float)(subdivisions + 1)) * (m_D - m_A);
+        //            Vector3 v6 = v2;
+
+        //            v1 /= v1.magnitude;
+        //            v2 /= v2.magnitude;
+        //            v3 /= v3.magnitude;
+        //            v4 /= v4.magnitude;
+        //            v5 /= v5.magnitude;
+        //            v6 /= v6.magnitude;
+
+        //            vertices.Add(v1);
+        //            vertices.Add(v2);
+        //            vertices.Add(v3);
+        //            vertices.Add(v4);
+        //            vertices.Add(v5);
+        //            vertices.Add(v6);
+        //        }
+        //    }
+
+        //    triangles = new List<int>(trianglesCount);
+        //    for (int i = 0; i != trianglesCount; i++)
+        //    {
+        //        triangles.Add(i + m_index * vertexCount);
+        //    }
+        //}
+
+
+        public void Spherify(int subdivisions, out List<Triangle> triangles)
+        {
+            triangles = new List<Triangle>();
 
             for (int i = 0; i != subdivisions + 1; i++)
             {
@@ -106,27 +153,40 @@ public class Stone : MonoBehaviour
                     v5 /= v5.magnitude;
                     v6 /= v6.magnitude;
 
-                    vertices.Add(v1);
-                    vertices.Add(v2);
-                    vertices.Add(v3);
-                    vertices.Add(v4);
-                    vertices.Add(v5);
-                    vertices.Add(v6);
+                    Vector3[] t1Vertices = new Vector3[3];
+                    t1Vertices[0] = v1;
+                    t1Vertices[1] = v2;
+                    t1Vertices[2] = v3;
+                    Vector3[] t2Vertices = new Vector3[3];
+                    t2Vertices[0] = v4;
+                    t2Vertices[1] = v5;
+                    t2Vertices[2] = v6;
+
+                    Triangle t1 = new Triangle(t1Vertices);
+                    Triangle t2 = new Triangle(t2Vertices);
+
+                    triangles.Add(t1);
+                    triangles.Add(t2);
                 }
             }
 
-            triangles = new List<int>(trianglesCount);
-            for (int i = 0; i != trianglesCount; i++)
-            {
-                triangles.Add(i + m_index * vertexCount);
-            }
+            //int vertexCount = (subdivisions + 1) * (subdivisions + 1) * 2 * 3;
+            //for (int i = 0; i != triangles.Count; i++)
+            //{
+            //    Triangle triangle = triangles[i];
+            //    int[] indices = new int[3];
+            //    indices[0] = 3 * i + m_index * vertexCount;
+            //    indices[1] = 3 * i + m_index * vertexCount + 1;
+            //    indices[2] = 3 * i + m_index * vertexCount + 2;
+            //    triangle.m_indices = indices;
+            //}
         }
     }
 
     /**
     * Spherify a cube by sampling each one of the cube faces
     **/
-    private PendingMesh SpherifyCube()
+    private void SpherifyCube()
     {
         //start by creating a cube
         Vector3[] cubeVertices = new Vector3[8];
@@ -147,42 +207,253 @@ public class Stone : MonoBehaviour
         CubeFace f6 = new CubeFace(5, cubeVertices[4], cubeVertices[5], cubeVertices[6], cubeVertices[7], Vector3.up);
 
         int subdivisions = 8;
-        List<Vector3> sphereVertices = new List<Vector3>(6 * (subdivisions + 2) * (subdivisions + 2));
-        List<int> sphereTriangles = new List<int>(6 * (subdivisions + 1) * (subdivisions + 1) * 2);
 
-        List<Vector3> faceVertices;
-        List<int> faceTriangles;
-        f1.Spherify(subdivisions, out faceVertices, out faceTriangles);
-        sphereVertices.AddRange(faceVertices);
-        sphereTriangles.AddRange(faceTriangles);
-        f2.Spherify(subdivisions, out faceVertices, out faceTriangles);
-        sphereVertices.AddRange(faceVertices);
-        sphereTriangles.AddRange(faceTriangles);
-        f3.Spherify(subdivisions, out faceVertices, out faceTriangles);
-        sphereVertices.AddRange(faceVertices);
-        sphereTriangles.AddRange(faceTriangles);
-        f4.Spherify(subdivisions, out faceVertices, out faceTriangles);
-        sphereVertices.AddRange(faceVertices);
-        sphereTriangles.AddRange(faceTriangles);
-        f5.Spherify(subdivisions, out faceVertices, out faceTriangles);
-        sphereVertices.AddRange(faceVertices);
-        sphereTriangles.AddRange(faceTriangles);
-        f6.Spherify(subdivisions, out faceVertices, out faceTriangles);
-        sphereVertices.AddRange(faceVertices);
-        sphereTriangles.AddRange(faceTriangles);
+        List<Triangle> pendingTriangles = new List<Triangle>();
+        List<Triangle> faceTriangles;
+        f1.Spherify(subdivisions, out faceTriangles);
+        pendingTriangles.AddRange(faceTriangles);
+        f2.Spherify(subdivisions, out faceTriangles);
+        pendingTriangles.AddRange(faceTriangles);
+        f3.Spherify(subdivisions, out faceTriangles);
+        pendingTriangles.AddRange(faceTriangles);
+        f4.Spherify(subdivisions, out faceTriangles);
+        pendingTriangles.AddRange(faceTriangles);
+        f5.Spherify(subdivisions, out faceTriangles);
+        pendingTriangles.AddRange(faceTriangles);
+        f6.Spherify(subdivisions, out faceTriangles);
+        pendingTriangles.AddRange(faceTriangles);
 
-        PendingMesh pendingMesh = new PendingMesh();
-        pendingMesh.m_vertices = sphereVertices;
-        pendingMesh.m_triangles = sphereTriangles;
+        m_pendingTriangles = new Triangle[Mathf.NextPowerOfTwo(pendingTriangles.Count)]; //enlarge the array to prevent array overflow when splitting triangles
 
-        return pendingMesh;
+        for (int i = 0; i != m_pendingTriangles.Length; i++)
+        {
+            if (i < pendingTriangles.Count)
+                m_pendingTriangles[i] = pendingTriangles[i];
+            else
+                m_pendingTriangles[i] = null;
+        }
     }
 
+    private int GetValidTrianglesCount()
+    {
+        int count = 0;
+        for (int i = 0; i != m_pendingTriangles.Length; i++)
+        {
+            if (m_pendingTriangles[i] != null && m_pendingTriangles[i].m_valid)
+                count++;
+        }
+
+        return count;
+    }
+
+    /**
+    * Refresh the mesh by filling in the vertices array using the array of pending triangles
+    **/
     private void InvalidateMesh()
     {
-        m_mesh.vertices = m_pendingMesh.m_vertices.ToArray();
-        m_mesh.triangles = m_pendingMesh.m_triangles.ToArray();
+        int validTrianglesCount = GetValidTrianglesCount();
+        Vector3[] meshVertices = new Vector3[3 * validTrianglesCount];
+        int[] meshIndices = new int[3 * validTrianglesCount];
+        Color[] meshColors = new Color[3 * validTrianglesCount];
+
+        int index = 0;
+        for (int i = 0; i != m_pendingTriangles.Length; i++)
+        {
+            Triangle triangle = m_pendingTriangles[i];
+            if (triangle == null || !triangle.m_valid)
+                continue;
+
+            meshVertices[3 * index] = triangle.m_points[0];
+            meshVertices[3 * index + 1] = triangle.m_points[1];
+            meshVertices[3 * index + 2] = triangle.m_points[2];
+
+            meshIndices[3 * index] = 3 * index;
+            meshIndices[3 * index + 1] = 3 * index + 1;
+            meshIndices[3 * index + 2] = 3 * index + 2;
+
+            meshColors[3 * index] = triangle.m_color;
+            meshColors[3 * index + 1] = triangle.m_color;
+            meshColors[3 * index + 2] = triangle.m_color;
+
+            index++;
+        }
+
+        m_mesh.Clear();
+        m_mesh.vertices = meshVertices;
+        m_mesh.triangles = meshIndices;
+        m_mesh.colors = meshColors;
         m_mesh.RecalculateBounds();
         m_mesh.RecalculateNormals();
+    }
+
+    /**
+    * Small struct to store a paire of intersection points that belong to the same triangle
+    **/
+    private class IntersectionPair
+    {
+        public Triangle.IntersectionPoint m_pointA;
+        public Triangle.IntersectionPoint m_pointB;
+        public bool m_valid;
+
+        public IntersectionPair(Triangle.IntersectionPoint pointA, Triangle.IntersectionPoint pointB)
+        {
+            m_pointA = pointA;
+            m_pointB = pointB;
+            m_valid = true;
+        }
+        
+        public override string ToString()
+        {
+            return m_valid.ToString();
+        }
+    }
+
+    /**
+    * Bisect a given volume once, return true if the volume was actually intersected
+    **/
+    public bool BisectOnce()
+    {
+        Plane bisectPlane = CreateBisectPlane(Vector3.up);
+        List<IntersectionPair> intersectionPairs = new List<IntersectionPair>();
+        
+        for (int i = 0; i != m_pendingTriangles.Length; i++)
+        {
+            Triangle triangle = m_pendingTriangles[i];
+            if (triangle == null || !triangle.m_valid)
+                continue;
+
+            Triangle.IntersectionPoint[] intersections;
+            if (triangle.IntersectsPlane(bisectPlane, out intersections))
+            {
+                intersectionPairs.Add(new IntersectionPair(intersections[0], intersections[1]));
+
+                Triangle[] splitTriangles;
+                triangle.Split(intersections, out splitTriangles);
+                
+                triangle.m_valid = false;
+
+                for (int m = 0; m != splitTriangles.Length; m++)
+                {
+                    //remove split triangles that are on the wrong side of the plane
+                    if (bisectPlane.GetSide(splitTriangles[m].m_center))
+                    {
+                        splitTriangles[m].m_valid = false;
+                        continue;
+                    }
+
+                    splitTriangles[m].m_color = Color.red;
+                }
+
+                InsertTriangles(splitTriangles);
+            }
+            else
+            {
+                if (bisectPlane.GetSide(triangle.m_center))
+                    triangle.m_valid = false;
+            }
+        }
+
+        intersectionPairs[0].m_pointB.ShareSamePosition(intersectionPairs[1].m_pointB);
+
+        if (intersectionPairs.Count > 2)
+        {
+            //build the new plane face of the stone
+            Triangle.IntersectionPoint[] planeFaceVertices = new Triangle.IntersectionPoint[intersectionPairs.Count];
+            planeFaceVertices[0] = intersectionPairs[0].m_pointA;
+            planeFaceVertices[1] = intersectionPairs[0].m_pointB;
+            intersectionPairs[0].m_valid = false;
+            Triangle.IntersectionPoint lastVertex = planeFaceVertices[1];
+
+            bool bShare = planeFaceVertices[1].ShareSamePosition(intersectionPairs[3].m_pointA);
+
+            int vertexIndex = 2;
+            while (vertexIndex < planeFaceVertices.Length)
+            {
+                bool bBreakLoop = true;
+                for (int p = 0; p != intersectionPairs.Count; p++)
+                {
+                    if (intersectionPairs[p].m_valid)
+                    {
+                        bool bShareSamePositionAsPointA = intersectionPairs[p].m_pointA.ShareSamePosition(lastVertex);
+                        bool bShareSamePositionAsPointB = false;
+                        if (!bShareSamePositionAsPointA)
+                            bShareSamePositionAsPointB = intersectionPairs[p].m_pointB.ShareSamePosition(lastVertex);
+
+                        if (bShareSamePositionAsPointA || bShareSamePositionAsPointB)
+                        {
+                            lastVertex = bShareSamePositionAsPointA ? intersectionPairs[p].m_pointB : intersectionPairs[p].m_pointA;
+                            planeFaceVertices[vertexIndex] = lastVertex;
+                            vertexIndex++;
+                            intersectionPairs[p].m_valid = false;
+
+                            bBreakLoop = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (bBreakLoop)
+                    break;
+            }
+
+            Triangle[] planeFaceTriangles = TriangulateBisectFace(planeFaceVertices);
+            InsertTriangles(planeFaceTriangles);
+
+            InvalidateMesh();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private Triangle[] TriangulateBisectFace(Triangle.IntersectionPoint[] faceVertices)
+    {
+        Triangle[] faceTriangles = new Triangle[faceVertices.Length - 2];
+        for (int i = 0; i != faceTriangles.Length; i++)
+        {
+            Vector3[] tVertices = new Vector3[3];
+            tVertices[0] = faceVertices[0].m_point;
+            tVertices[1] = faceVertices[i + 2].m_point;
+            tVertices[2] = faceVertices[i + 1].m_point;
+            faceTriangles[i] = new Triangle(tVertices);
+            faceTriangles[i].m_color = Color.yellow;
+        }
+
+        return faceTriangles;
+    }
+
+    /**
+    * Insert new triangles in the m_pendingTriangles array, by replacing invalid triangles or adding to the end of the array if no slot is available
+    **/
+    private void InsertTriangles(Triangle[] triangles)
+    {
+        for (int p = 0; p != triangles.Length; p++)
+        {
+            if (!triangles[p].m_valid)
+                continue;
+
+            for (int q = 0; q != m_pendingTriangles.Length; q++)
+            {
+                if (m_pendingTriangles[q] == null || !m_pendingTriangles[q].m_valid)
+                {
+                    m_pendingTriangles[q] = triangles[p];
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+    * Create a plane using a normal and a point that belongs to this plane
+    **/
+    private Plane CreateBisectPlane(Vector3 normal, float nearClip = 0.25f, float farClip = 0.85f)
+    {
+        //TODO randomize a point between nearClip and farClip along the normal
+        //for now just set a constant value
+        float t = 0.75f;
+        Vector3 planePoint = t * normal;
+
+        return new Plane(normal, planePoint);
     }
 }
---
+++ Assets/Scripts/Triangle.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle
+{
+    public Vector3[] m_points;
+    //public int[] m_indices;
+    public Color m_color;
+    public Vector3 m_center;
+    public Vector3 m_normal;
+
+    public bool m_valid;
+    
+    public Triangle(Vector3[] points/*, int[] indices*/)
+    {
+        m_points = points;
+        //m_indices = indices;
+        m_color = Color.blue;
+
+        m_center = (points[0] + points[1] + points[2]) / 3.0f;
+
+        m_normal = Vector3.Cross(points[2] - points[0], points[1] - points[0]);
+        m_normal.Normalize();
+
+        m_valid = true;
+    }
+
+    /**
+    * Return the previous vertex in clockwise order
+    **/
+    private Vector3 GetCWPreviousVertex(int vertexIndex)
+    {
+        return m_points[vertexIndex == 0 ? 2 : vertexIndex - 1];
+    }
+
+    /**
+    * Return the next vertex in clockwise order
+    **/
+    private Vector3 GetCWNextVertex(int vertexIndex)
+    {
+        return m_points[vertexIndex == 2 ? 0 : vertexIndex + 1];
+    }
+
+    public bool IsFlat()
+    {
+        Vector3 u = m_points[1] - m_points[0];
+        Vector3 v = m_points[2] - m_points[0];
+
+        return Vector3.Cross(u, v) == Vector3.zero;
+    }
+
+    public struct IntersectionPoint
+    {
+        public Vector3 m_point;
+        public int m_edgeIndex; //the edge index i.e the index of the first point of this edge when wheeling the triangle in cw order
+        public bool m_isTriangleVertex; //is this intersection point one of the three triangle vertices
+
+        public IntersectionPoint(Vector3 point, int edgeIndex, bool isTriangleVertex)
+        {
+            m_point = point;
+            m_edgeIndex = edgeIndex;
+            m_isTriangleVertex = isTriangleVertex;
+        }
+
+        public bool ShareSamePosition(IntersectionPoint other)
+        {
+            return (m_point - other.m_point).sqrMagnitude < 1E-010;
+        }
+    }
+
+    public bool IntersectsPlane(Plane plane, out IntersectionPoint[] intersections)
+    {
+        List<int> pointsOnPlane = new List<int>();
+        if (plane.GetDistanceToPoint(m_points[0]) == 0)
+            pointsOnPlane.Add(0);
+        if (plane.GetDistanceToPoint(m_points[1]) == 0)
+            pointsOnPlane.Add(1);
+        if (plane.GetDistanceToPoint(m_points[2]) == 0)
+            pointsOnPlane.Add(2);
+
+        if (pointsOnPlane.Count >= 2)
+        {
+            intersections = null;
+            return false;
+        }
+        else if (pointsOnPlane.Count == 1)
+        {
+            //test if the edge facing that point is intersected by the plane
+            Vector3 edgePoint1, edgePoint2;
+            if (pointsOnPlane[0] == 0)
+            {
+                edgePoint1 = m_points[1];
+                edgePoint2 = m_points[2];
+            }
+            else if (pointsOnPlane[0] == 1)
+            {
+                edgePoint1 = m_points[2];
+                edgePoint2 = m_points[0];
+            }
+            else
+            {
+                edgePoint1 = m_points[0];
+                edgePoint2 = m_points[1];
+            }
+
+            Ray ray = new Ray(edgePoint1, edgePoint2 - edgePoint1);
+            float rayDistance;
+            if (plane.Raycast(ray, out rayDistance))
+            {
+                float sqrDistance = rayDistance * rayDistance;
+                if (sqrDistance > 0 && sqrDistance < (edgePoint2 - edgePoint1).sqrMagnitude)
+                {
+                    intersections = new IntersectionPoint[2];
+                    intersections[0] = new IntersectionPoint(m_points[pointsOnPlane[0]], pointsOnPlane[0], true);
+                    intersections[1] = new IntersectionPoint(ray.GetPoint(rayDistance), pointsOnPlane[0] < 2 ? pointsOnPlane[0] + 1 : 0, false);
+                    return true;
+                }
+            }
+
+            intersections = null;
+            return false;
+        }
+        else //no points on the plane, either two intersections or no intersection
+        {
+            intersections = new IntersectionPoint[2];
+            int index = 0;
+
+            for (int i = 0; i != m_points.Length; i++)
+            {
+                Vector3 point1 = m_points[i];
+                Vector3 point2 = m_points[i == 2 ? 0 : i + 1];
+
+                Ray ray = new Ray(point1, point2 - point1);
+                float rayDistance;
+                if (plane.Raycast(ray, out rayDistance))
+                {
+                    float sqrDistance = rayDistance * rayDistance;
+                    if (sqrDistance > 0 && sqrDistance < (point2 - point1).sqrMagnitude)
+                    {
+                        intersections[index] = new IntersectionPoint(ray.GetPoint(rayDistance), i, false);
+                        index++;
+                    }
+                }
+            }
+
+            if (index == 1)
+                intersections = null;
+
+            return (index == 2);
+        }       
+    }
+
+    /**
+    * Split this triangle
+    **/
+    public void Split(IntersectionPoint[] intersectionPoints, out Triangle[] splitTriangles)
+    {
+        if (intersectionPoints[0].m_isTriangleVertex)
+        {
+            Vector3[] t1Vertices = new Vector3[3];
+            t1Vertices[0] = intersectionPoints[0].m_point;
+            t1Vertices[1] = GetCWNextVertex(intersectionPoints[0].m_edgeIndex);
+            t1Vertices[2] = intersectionPoints[1].m_point;
+            Triangle t1 = new Triangle(t1Vertices);
+
+            Vector3[] t2Vertices = new Vector3[3];
+            t2Vertices[0] = GetCWPreviousVertex(intersectionPoints[0].m_edgeIndex); //previous vertex
+            t2Vertices[1] = intersectionPoints[0].m_point;
+            t2Vertices[2] = intersectionPoints[1].m_point;
+            Triangle t2 = new Triangle(t2Vertices);
+
+            splitTriangles = new Triangle[2];
+            splitTriangles[0] = t1;
+            splitTriangles[1] = t2;
+        }
+        else
+        {
+            //sort intersection points in ascending order so their edgeIndex are consecutive
+            if (intersectionPoints[0].m_edgeIndex == intersectionPoints[1].m_edgeIndex + 1 || intersectionPoints[0].m_edgeIndex == 0 && intersectionPoints[1].m_edgeIndex == 2)
+            {
+                IntersectionPoint tmp = intersectionPoints[0];
+                intersectionPoints[0] = intersectionPoints[1];
+                intersectionPoints[1] = tmp;
+            }
+
+            Vector3[] t1Vertices = new Vector3[3];
+            t1Vertices[0] = intersectionPoints[0].m_point;
+            t1Vertices[1] = GetCWNextVertex(intersectionPoints[0].m_edgeIndex);
+            t1Vertices[2] = intersectionPoints[1].m_point;
+            Triangle t1 = new Triangle(t1Vertices);
+
+            Vector3[] t2Vertices = new Vector3[3];
+            t2Vertices[0] = m_points[intersectionPoints[0].m_edgeIndex];
+            t2Vertices[1] = intersectionPoints[0].m_point;
+            t2Vertices[2] = intersectionPoints[1].m_point;
+            Triangle t2 = new Triangle(t2Vertices);
+
+            Vector3[] t3Vertices = new Vector3[3];
+            t3Vertices[0] = m_points[intersectionPoints[0].m_edgeIndex];
+            t3Vertices[1] = intersectionPoints[1].m_point;
+            t3Vertices[2] = GetCWNextVertex(intersectionPoints[1].m_edgeIndex);
+            Triangle t3 = new Triangle(t3Vertices);
+
+            //if (t1.IsFlat() ||
+            //    t2.IsFlat() ||
+            //    t3.IsFlat())
+            //    Debug.Log("flat");
+
+            splitTriangles = new Triangle[3];
+            splitTriangles[0] = t1;
+            splitTriangles[1] = t2;
+            splitTriangles[2] = t3;
+        }
+    }
+}
